ff9b25bef65c6978ff6b66112acb3bf135c59ba6
LianoQ <liano.quini@gmail.com>
2018-02-14 11:26:56 +0100

    Finished our Loco framework

---
+++ LocoFramework/LocoFramework/LocoFramework.h
@@ -0,0 +1,19 @@
+//
+//  LocoFramework.h
+//  LocoFramework
+//
+//  Created by Imac27 on 14/02/2018.
+//  Copyright © 2018 LianoQ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LocoFramework.
+FOUNDATION_EXPORT double LocoFrameworkVersionNumber;
+
+//! Project version string for LocoFramework.
+FOUNDATION_EXPORT const unsigned char LocoFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LocoFramework/PublicHeader.h>
+
+
---
+++ LocoFramework/LocoFramework/Service.swift
@@ -0,0 +1,23 @@
+//
+//  Service.swift
+//  LocoFramework
+//
+//  Created by Imac27 on 14/02/2018.
+//  Copyright © 2018 LianoQ. All rights reserved.
+//
+
+
+
+import Foundation
+
+
+public class Service {
+    
+    private init(){}
+    
+    public static func doSomething() -> String{
+        return "Did some stuff, here you go"
+    }
+    
+}
+
